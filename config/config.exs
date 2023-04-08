fdbbd9ed8bd51ec047536e488d62c14328fff3c9
Juan Lovera <juanma.lovera@hotmail.com>
2023-04-08 14:00:55 -0500

    Setup waffle and project domain

--- config/config.exs
+++ config/config.exs
@@ -59,6 +59,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Waffle storage setup
+config :waffle,
+  storage: Waffle.Storage.Local,
+  storage_dir_prefix: "priv/static/images"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias BlogApp.Posts
+
+Posts.create_post(%{image: "test/support/files/image_test.png"})
